611974dad27c72f1fbb2e773145640b3d5056511
Antonio Muñoz <antoniogmc@gmail.com>
2025-01-18 17:40:52 +0100

    add mill

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 set -Eeuo pipefail
 shopt -s expand_aliases
 
-rm -rf bin && mkdir -p bin && javac --enable-preview --source 23 -Xlint:all -d bin snippets/*.java
-
 alias marp="docker run --rm -v $PWD:/home/marp/app/ -e MARP_USER="$(id -u):$(id -g)" -e LANG=$LANG marpteam/marp-cli"
 
 marp slides.md -o slides.html
