bf91a5121d7c42294cbefb402fd5a14235600671
Antonio Muñoz <antonio.munoz@clarity.ai>
2025-01-30 16:58:09 +0100

    fix install

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -Eeuo pipefail
 
 sudo mkdir -p /srv/http/commitconf25/images
 sudo cp slides.{html,pdf} /srv/http/commitconf25/
-sudo cp images/*.jpg /srv/http/commitconf25/images/
+sudo cp images/*.{jpg,png} /srv/http/commitconf25/images/
 
 echo done
