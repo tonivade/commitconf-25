70a3f9ef42e0fb4fa7e8eeecd808ce67f6c36b5b
Antonio Muñoz <antonio.munoz@clarity.ai>
2025-02-07 15:03:49 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -Eeuo pipefail
 
-npm i
-marp --engine ./engine.mjs --theme ./themes/custom.css slides.md
+npm i || exit 1
+node_modules/.bin/marp --engine ./engine.mjs --theme ./themes/custom.css slides.md
