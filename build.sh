8fca52b20c5c09b312ab61a7d4ee04a80781ad77
Antonio Muñoz <antoniogmc@gmail.com>
2025-02-08 22:56:51 +0100

    minor change

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 
 npm i || exit 1
-node_modules/.bin/marp --engine ./engine.mjs --theme ./themes/custom.css slides.md
+node node_modules/.bin/marp --engine ./engine.mjs --theme ./themes/custom.css slides.md
