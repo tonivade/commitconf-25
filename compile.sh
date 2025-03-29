e9528e62d6ae30c271b9cf40e68e11f9c44e41fe
Antonio Muñoz <antoniogmc@gmail.com>
2025-03-29 15:54:08 +0100

    java 24

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 source $HOME/.sdkman/bin/sdkman-init.sh && sdk env
 
 rm -rf bin && mkdir -p bin && 
-    javac --enable-preview --source 23 -Xlint:all -d bin snippets/*.java && 
-    javac --enable-preview --source 23 -Xlint:all -d bin snippets/**/*.java && 
+    javac --enable-preview --source 24 -Xlint:all -d bin snippets/*.java && 
+    javac --enable-preview --source 24 -Xlint:all -d bin snippets/**/*.java && 
     echo build done
